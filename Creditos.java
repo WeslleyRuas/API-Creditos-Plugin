65572cf9b6b4d315c0f5713b8dac23c563807832
WeslleyRuas <midasgh3@gmail.com>
2016-08-12 09:27:07 -0300

    Update Creditos.java

--- Creditos.java
+++ Creditos.java
@@ -46,45 +46,61 @@ public class Creditos {
 		/* Se o arquivo creditos.txt existir ele checa se o arquivo esta sem os creditos devidos */
         else
         {
-			try {
-				String line1 = (String) FileUtils.readLines(file).get(0);
-				String line2 = (String) FileUtils.readLines(file).get(1);
-				String line3 = (String) FileUtils.readLines(file).get(2);
-				if(!line1.equalsIgnoreCase("Criador : Weslley Ruas") || line2.equalsIgnoreCase("FB: https://www.facebook.com/weslleysaantoss") || line3.equalsIgnoreCase("GamersBoard Profile: http://gamersboard.com.br/user/12344-weslleyruas/"))
-				{
-					file.delete();
-					System.out.println("Creditos nao detectado, deletando e criando novamente!");
-					try 
-		        	{
-		        		escrever = new BufferedWriter(new FileWriter(file));
-		        		escrever.write("Criador : Weslley Ruas");
-		        		escrever.newLine();
-		        		escrever.write("FB: https://www.facebook.com/weslleysaantoss");
-		        		escrever.newLine();
-		        		escrever.write("GamersBoard Profile: http://gamersboard.com.br/user/12344-weslleyruas/");
-		        	} 
-		        	catch ( IOException e ) 
-		        	{
-		        		e.printStackTrace();
-		        	} 
-		        	finally 
-		        	{
-		        		if ( escrever != null ) 
-		        		{
-		        			try 
-		        			{
-		        				escrever.close();
-		        			} 
-		        			catch (IOException e) 
-		        			{
-		        				e.printStackTrace();
-		        			}
-		        		}
-		        	}
+				try {
+					String line1 = (String) FileUtils.readLines(file).get(0);
+					String line2 = (String) FileUtils.readLines(file).get(1);
+					String line3 = (String) FileUtils.readLines(file).get(2);
+					Boolean check = false;
+					if(!line1.equalsIgnoreCase("Criador : Weslley Ruas"))
+					{
+						System.out.println("A linha 1 foi modificada, os creditos serao retornados para o original!");
+						check = true;
+					}
+					if(!line2.equalsIgnoreCase("FB: https://www.facebook.com/weslleysaantoss"))
+					{
+						System.out.println("A linha 2 foi modificada, os creditos serao retornados para o original!");
+						check = true;
+					}
+					if(!line3.equalsIgnoreCase("GamersBoard Profile: http://gamersboard.com.br/user/12344-weslleyruas/"))
+					{
+						System.out.println("A linha 3 foi modificada, os creditos serao retornados para o original!");
+						check = true;
+					}
+					if(check == true)
+					{
+						file.delete();
+						try 
+			        	{
+			        		escrever = new BufferedWriter(new FileWriter(file));
+			        		escrever.write("Criador : Weslley Ruas");
+			        		escrever.newLine();
+			        		escrever.write("FB: https://www.facebook.com/weslleysaantoss");
+			        		escrever.newLine();
+			        		escrever.write("GamersBoard Profile: http://gamersboard.com.br/user/12344-weslleyruas/");
+			        	} 
+			        	catch ( IOException e ) 
+			        	{
+			        		e.printStackTrace();
+			        	} 
+			        	finally 
+			        	{
+			        		if ( escrever != null ) 
+			        		{
+			        			try 
+			        			{
+			        				escrever.close();
+			        			} 
+			        			catch (IOException e) 
+			        			{
+			        				e.printStackTrace();
+			        			}
+			        		}
+			        	}
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			
         }
 	}
 }
